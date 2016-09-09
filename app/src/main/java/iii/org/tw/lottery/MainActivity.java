32dcd17e93fe2eda91e10a3b7c13b89e0a6530f9
Brad Chao <brad@brad.tw>
2016-09-09 16:26:18 +0800

    v6

--- app/src/main/java/iii/org/tw/lottery/MainActivity.java
+++ app/src/main/java/iii/org/tw/lottery/MainActivity.java
@@ -12,10 +12,16 @@ public class MainActivity extends AppCompatActivity {
     private View guess;
     private EditText input;
     private TextView info;
+    private String answer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        answer = createAnswer(3);
+
+        input = (EditText)findViewById(R.id.input);
+        info = (TextView)findViewById(R.id.infoText);
 
         guess = findViewById(R.id.guess);
         guess.setOnClickListener(new View.OnClickListener() {
@@ -28,7 +34,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void doGuess(){
-        
+        String strInput = input.getText().toString();
+        String result = checkAB(answer, strInput);
+        info.append(strInput + ":" + result + "\n");
+        input.setText("");
     }
+
+    static String createAnswer(int n){
+        // 洗牌
+        int[] poker = new int[n];
+        for (int i=0; i<poker.length; i++){
+            int temp;
+            // 檢查機制
+            boolean isRepeat;
+            do {
+                temp = (int)(Math.random()*10);
+                isRepeat = false;
+                for (int j=0; j<i; j++){
+                    if (temp == poker[j]){
+                        isRepeat = true;
+                        break;
+                    }
+                }
+            }while(isRepeat);
+            poker[i] = temp;
+        }
+        String ret = "";
+        for (int p : poker)ret += p;
+
+        return ret;
+    }
+    static String checkAB(String a, String g){
+        int A, B; A = B = 0;
+        for (int i=0; i<g.length(); i++){
+            if (g.charAt(i) == a.charAt(i)){
+                A++;
+            }else if (a.indexOf(g.charAt(i))!=-1){
+                B++;
+            }
+        }
+        return A + "A" + B + "B";
+    }
+
+
 
 }
