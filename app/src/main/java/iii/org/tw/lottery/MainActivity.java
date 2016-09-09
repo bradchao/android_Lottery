11dfdafb3bfb722e73fc9b024edd641907099125
Brad Chao <brad@brad.tw>
2016-09-09 15:02:59 +0800

    v3

--- app/src/main/java/iii/org/tw/lottery/MainActivity.java
+++ app/src/main/java/iii/org/tw/lottery/MainActivity.java
@@ -8,22 +8,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button click;
-    private TextView mesg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        click = (Button)findViewById(R.id.bntClick);
-        mesg = (TextView)findViewById(R.id.mesg);
-
-        click.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mesg.setText("Lottery:" + (int)(Math.random()*49+1));
-            }
-        });
 
     }
 }
