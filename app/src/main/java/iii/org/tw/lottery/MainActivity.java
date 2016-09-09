29d9ccce7b0472397ed9ff42a2047c90cb2cb7bc
Brad Chao <brad@brad.tw>
2016-09-09 15:55:18 +0800

    v6

--- app/src/main/java/iii/org/tw/lottery/MainActivity.java
+++ app/src/main/java/iii/org/tw/lottery/MainActivity.java
@@ -5,13 +5,30 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private View guess;
+    private EditText input;
+    private TextView info;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        guess = findViewById(R.id.guess);
+        guess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doGuess();
+            }
+        });
+
     }
+
+    private void doGuess(){
+        
+    }
+
 }
